050cc30a9229bde87830e69ae63e9d68b059bd22
baw-george <georgealcoforado2006@comcast.net>
2012-10-26 23:31:32 -0400

    add day 6 and 7 HW

---
+++ discount_with_form.php
@@ -0,0 +1,59 @@
+<?php
+     // Get the item price from the form
+     
+     $item_price = $_POST["item_price"];
+     
+     // Calculate the discounted cost
+     
+     if ($item_price <= 10.00)
+        {
+     	   $discount_percent = 10;
+        }
+     else
+       {
+     	   $discount_percent = 20;
+        }
+     
+     $list_price = number_format($item_price, 2);
+     
+     $discount = $list_price * $discount_percent * 0.01;
+     
+     $discounted_cost = $list_price - $discount;
+          
+     $list_price_formatted = "$".$list_price;
+     $discount_percent_formatted = $discount_percent."%";
+     $discount_formatted = "$".number_format($discount,2);
+     $discounted_cost_formatted = "$".number_format($discounted_cost,2);
+     
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Discount Calculator</title>
+    <link rel="stylesheet" type="text/css" href="css/hwwk6.css" />
+</head>
+<body>
+  <section id="container">
+    <div id="content">
+        <h1>Here is your discount information</h1>
+
+        <label>Item Price:</label>
+        <span><?php echo $list_price_formatted; ?></span><br />
+
+        <label>Discount:</label>
+        <span><?php echo $discount_percent_formatted; ?></span><br />
+
+        <label>Discount Amount:</label>
+        <span><?php echo $discount_formatted; ?></span><br />
+
+        <label>Discounted Cost:</label>
+        <span><?php echo $discounted_cost_formatted; ?></span><br />
+
+        <p>&nbsp;</p>
+    </div>
+  </section>
+</body>
+
+</html>
---
+++ times_table_with_form.php
@@ -0,0 +1,34 @@
+<?php
+     // Get the number for which the times table is to be generated
+     
+     $tt_number = $_POST["tt_number"];
+     
+     $t_num = number_format($tt_number,0);
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Times Table</title>
+    <link rel="stylesheet" type="text/css" href="css/hwwk6.css" />
+</head>
+<body>
+  <section id="container">
+    <div id="content">
+        <h1>Here is the times table</h1>
+
+        <span><?php 
+        for ($i = 1; $i < 13; $i = $i + 1)
+        {
+        	echo $t_num . " x " . $i . " = " . $t_num * $i;
+        	echo "<br />";
+        }
+        ?></span><br />
+
+        <p>&nbsp;</p>
+    </div>
+  </section>
+</body>
+
+</html>
